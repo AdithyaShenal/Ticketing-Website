53d400511bc34609a1d93870dc13adb8bf47a6d3
BhawaniPrabhasha <prabhashadilrangi1111@gmail.com>
2025-07-27 21:55:01 +0530

    footer component

---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+
+<footer class="footer">
+  <div class="newsletter">
+    <input type="email" placeholder="Your email address">
+    <button>⏎</button>
+
+  </div>
+  <div class="footer-columns">
+    <div>
+      <h4>About us</h4>
+      <p>About<br>News<br>Support</p>
+    </div>
+    <div>
+      <h4>Follow us</h4>
+      <p>
+        <a href="https://facebook.com" target="_blank">Facebook</a><br>
+        <a href="https://instagram.com" target="_blank">Instagram</a><br>
+        
+      </p>
+    </div>
+    
+  </div>
+  <p class="copyright">Ticketist &copy; <?php echo date('Y'); ?></p>
+</footer>
